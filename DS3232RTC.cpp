51d50622f3532db1dc21c0328ac5edbb8efdb162
JChristensen <j.christensen@sbcglobal.net>
2014-02-21 21:10:11 -0500

    Fixes #2. Move I2C defines from .h file to .cpp file to avoid including unneeded libraries.

--- DS3232RTC.cpp
+++ DS3232RTC.cpp
@@ -31,13 +31,38 @@
  * This work is licensed under the Creative Commons Attribution-        *
  * ShareAlike 3.0 Unported License. To view a copy of this license,     *
  * visit http://creativecommons.org/licenses/by-sa/3.0/ or send a       *
- * letter to Creative Commons, 171 Second Street, Suite 300,            *
- * San Francisco, California, 94105, USA.                               *
+ * letter to Creative Commons, 444 Castro Street, Suite 900,            *
+ * Mountain View, CA 94041.                                             *
  *----------------------------------------------------------------------*/ 
 
-#include <Wire.h>
-#include <TinyWireM.h>
 #include <DS3232RTC.h>
+
+//define release-independent I2C functions
+#if defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+#include <TinyWireM.h>
+#define i2cBegin TinyWireM.begin
+#define i2cBeginTransmission TinyWireM.beginTransmission
+#define i2cEndTransmission TinyWireM.endTransmission
+#define i2cRequestFrom TinyWireM.requestFrom
+#define i2cRead TinyWireM.receive
+#define i2cWrite TinyWireM.send
+#elif ARDUINO >= 100
+#include <Wire.h>
+#define i2cBegin Wire.begin
+#define i2cBeginTransmission Wire.beginTransmission
+#define i2cEndTransmission Wire.endTransmission
+#define i2cRequestFrom Wire.requestFrom
+#define i2cRead Wire.read
+#define i2cWrite Wire.write
+#else
+#include <Wire.h>
+#define i2cBegin Wire.begin
+#define i2cBeginTransmission Wire.beginTransmission
+#define i2cEndTransmission Wire.endTransmission
+#define i2cRequestFrom Wire.requestFrom
+#define i2cRead Wire.receive
+#define i2cWrite Wire.send
+#endif
 
 /*----------------------------------------------------------------------*
  * Constructor.                                                         *
